578d7678b897a408ac516030cb2e9d400a11845b
Robert Turner <47613450+rturner1989@users.noreply.github.com>
2024-05-01 13:54:02 +0100

    Update rollback.sh

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Template App ROLLBACK Database"
+container_id=$($(dirname "$0")/get_app_container_id.sh)
+
+echo "ROLLBACK app container with id $container_id..."
 echo "================="
+
 docker compose run --rm app rails db:rollback
 
 echo "Finished!"
