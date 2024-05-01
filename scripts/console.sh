c60586158508fd3b8bc06b6db7dd19c83ed0a3d1
Robert Turner <47613450+rturner1989@users.noreply.github.com>
2024-05-01 13:52:53 +0100

    Update console.sh

--- scripts/console.sh
+++ scripts/console.sh
@@ -1,3 +1,9 @@
-echo "Connecting to Template Console"
+#!/bin/bash
+
+# Run the id fetching script in this same folder ($0 => current script filepath)
+container_id=$($(dirname "$0")/get_app_container_id.sh)
+
+echo "Running rails console on app container with id $container_id..."
 echo "================="
-docker exec -it template-app-1 rails c
+
+docker exec -it $container_id rails c
