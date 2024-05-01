47a3cf11b21c400056a8dcd1770bf4d4dd8f857f
Robert Turner <47613450+rturner1989@users.noreply.github.com>
2024-05-01 13:53:53 +0100

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Template App MIGRATING Database"
+container_id=$($(dirname "$0")/get_app_container_id.sh)
+
+echo "MIGRATE app container with id $container_id..."
 echo "================="
+
 docker compose run --rm app rails db:migrate
 
 echo "Finished!"
