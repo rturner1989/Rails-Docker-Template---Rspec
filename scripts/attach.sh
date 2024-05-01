63559587e811c1ca86ebe8c5934a2520b1c8e541
Robert Turner <47613450+rturner1989@users.noreply.github.com>
2024-05-01 13:52:28 +0100

    Update attach.sh

--- scripts/attach.sh
+++ scripts/attach.sh
@@ -1,2 +1,9 @@
-name=$(docker ps --filter name=template-app-1 --format "{{.Names}}")
-docker attach $name
+#!/bin/bash
+
+# Run the id fetching script in this same folder ($0 => current script filepath)
+container_id=$($(dirname "$0")/get_app_container_id.sh)
+
+echo "Attaching to app container with id $container_id..."
+echo "================="
+
+docker attach $container_id
